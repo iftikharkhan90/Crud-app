28c67e655ec1129579feb6b8fb443cbc230547c8
Iftikhar Rana <iffirana708@gmail.com>
2019-12-18 03:48:49 +0500

    remove bugs deploy on heroku

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
-mongoose.connect("mongodb://iffi rana:iffi12345@ds123400.mlab.com:23400/crud", {  useNewUrlParser: true })
+mongoose.connect("mongodb://127.0.0.1/crud", { useCreateIndex: true, useNewUrlParser: true })
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'DB connection error:'));
 db.once('open', function () { console.log('Successfully connected to DB') });
