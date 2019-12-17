3fd7df97129600025f359bf86ec3c7f265b1768e
Iftikhar Rana <iffirana708@gmail.com>
2019-12-18 01:57:05 +0500

    adding cors

--- src/server.js
+++ src/server.js
@@ -23,7 +23,16 @@ var corsOptions = {
 }
 server.use(cors(corsOptions))
 
-
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+server.configure(function() {
+    server.use(allowCrossDomain);
+    //some other code
+}); 
 //Body Parser
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
