cf2e478b73e3a055b729d9e1bc649cd2b130c03e
Iftikhar Rana <iffirana708@gmail.com>
2019-12-18 00:39:51 +0500

    chaning

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
-mongoose.connect("mongodb://iffi rana:iffi12345@ds123400.mlab.com:23400/crud", { useCreateIndex: true, useNewUrlParser: true })
+mongoose.connect("mongodb://iffi rana:iffi12345@ds123400.mlab.com:23400/crud", {  useNewUrlParser: true })
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'DB connection error:'));
 db.once('open', function () { console.log('Successfully connected to DB') });
