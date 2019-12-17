b9d77c0f8062f52661fafeff809be213575818b6
Iftikhar Rana <iffirana708@gmail.com>
2019-12-18 03:30:05 +0500

    bug remove

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ require('./config/db-config')
 // Cors for local development
 
 
-app.use((req, res, next) => {
+server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header("Access-Control-Allow-Credentials", "true");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
